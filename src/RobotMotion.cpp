1d0f9155b1af790f7084c984a788f33a567b55b9
FilippoSalterini <s4516129@studenti.unige.it>
2025-01-06 15:29:23 +0000

    Starting developing RobotMotion.cpp

---
+++ src/RobotMotion.cpp
@@ -0,0 +1,3 @@
+/*Implement in ROS2, and with the new robot and simulation environment, the same behaviour implemented with the turtlesim.
+The robot is already spawned in (2,2), so there is no need to use services here! You just need to develop a ROS2 package with
+one node capable of moving the robot around in the simulation environment*/
