9fab8741fa2db3123749d2cc33d1ef18df38cde6
FilippoSalterini <s4516129@studenti.unige.it>
2025-01-13 10:49:47 +0100

    Update RobotMotion.cpp COMMIT FOR EXAM RT

--- src/RobotMotion.cpp
+++ src/RobotMotion.cpp
@@ -5,11 +5,21 @@ one node capable of moving the robot around in the simulation environment*/
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <nav_msgs/msg/odometry.hpp>
+
+#include "std_msgs/msg/Float32MultiArray.h" //examRT (check later)
+#include <std_srvs/srv/SetBool.hpp> //for the service part
+
 #include <iostream>
 #include <thread>
 #include <chrono>
 
 using namespace std;
+
+//examRT
+//here some gbal variables to store values
+double prev_x = 0.0;
+double prev_y = 0.0;
+
 
 //subsriber to print robot position and velocity
 void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
@@ -22,63 +32,59 @@ void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
     //get robot's velocity from the message (L and A)
     double linear_velocity = msg->twist.twist.linear.x;  // Linear velocity in x direction
     double angular_velocity = msg->twist.twist.angular.z; // Angular velocity (rotation)
+    
+      //ExamRT
+      //function for converting feet in mt
+double meterstoFeet(double meters) {
+      return meters * 3.28084;
+      }
+    
+      //ExamRT
+      double dx = metersToFeet(x - prev_x);
+      double dy = metersToFeet(y - prev_y);
+      auto position_msg = std_msgs::msg::Float32MultiArray(); 
+      position_msg.data.push_back(dx); 
+      position_msg.data.push_back(dy); 
+
+     position_pub->publish(position_msg);
 
     // Print the position and velocity
     cout << "[Robot Position]\n X: " << x << ", Y: " << y << ", Z: " << z << endl;
     cout << "[Robot Velocity]\n Linear: " << linear_velocity << ", Angular: " << angular_velocity<< endl;
+    cout << "[Change of the position (feet)]\n Delta X: " << dx << ", Delta Y: " << dy << endl;
+
 }
 
-void robot_motion(rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub) {
+//examRT
+// (check better later)
+bool robot_control_callback(const std_srvs::srv::SetBool::Request::SharedPtr req, std_srvs::srv::SetBool::Response::SharedPtr res) {
+  auto msg = geometry_msgs::msg::Twist();
 
-    auto msg = geometry_msgs::msg::Twist();
+  if (req->data) {
+    //here start the robot
+    //check here
 
-    string direction;
-    double speed = 0.2; //checked on internet is a default speed for robots (?)
-    
-    cout <<"LIST OF POSSIBLE DIRECTIONS \n"<< endl;
-    cout <<" [forward]\n [backward]\n [left]\n [right]\n"<<endl; 
-    
-    cout << "Enter the direction of the robot: ";
-    cin >> direction;    
-
-    cout << "Enter the speed (default 0.2 m/s): ";
-    cin >> speed;
-     
-    if (direction == "forward")
-    {
-        msg.linear.x = speed;
-    }
-    else if (direction == "backward")
-    {
-        msg.linear.x = -speed;
-    }
-    else if (direction == "left")
-    {
-        msg.linear.x = 1;        // Stop moving forward/backward
-        msg.angular.z = speed; 
-    }
-    else if (direction == "right")
-    {
-        msg.linear.x = 1;        // Stop moving forward/backward
-        msg.angular.z = -speed; 
-    }
-    else
-    {
-        cout << "Invalid direction entered, defaulting to forward." << endl;
-        msg.linear.x = speed;
-    }
-    // Publish the movement message
-    cmd_vel_pub->publish(msg);
-    
-    // 5 sec sleep
-    this_thread::sleep_for(std::chrono::seconds(5));
-    
-    // Stop the robot after 5 seconds
+  } else {
+    //here stop the robot
+    cout << "Stopping the robot" << endl;
     msg.linear.x = 0;
     msg.angular.z = 0;
-    
-    cmd_vel_pub->publish(msg); // Publish stop command
+  }
+
+  // Publish the stop/start command
+  cmd_vel_pub->publish(msg);
+
+  res->success = true;
+  return true;
 }
+
+
+//examRT
+//function for converting feet in mt
+double meterstoFeet(double meters) {
+  return meters * 3.28084;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -90,6 +96,20 @@ int main(int argc, char **argv)
     //publisher for the 'cmd_vel' topic
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub =
         node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+        
+        
+        
+    //examRT   
+    //publisher for the robot position in feet
+    rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr position_pub = 
+    node->create_publisher<std_msgs::msg::Float32MultiArray>("robot_position_feet", 10);
+    
+    //service for starting and stopping it (check)
+    rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr robot_control_service =
+      node->create_service<std_srvs::srv::SetBool>(
+          "robot_control", &robot_control_callback); 
+
+    
     
     //subscriber for the 'odom' topic
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub =
